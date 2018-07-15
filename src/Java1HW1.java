c4aab68658c85fd168b42dac73b5d567a2917e72
Irina Poberey <iriney_91@mail.ru>
2018-07-15 18:04:45 +0300

    initial commit

---
+++ src/Java1HW1.java
@@ -0,0 +1,90 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Java1. Homework1
+ * @authors Irina Poberey
+ * @version dated Jul 10, 2018
+ */
+public class Java1HW1 {
+
+    byte a1 = 3;
+    short a2 = 5;
+    int a3 = 59;
+    long a4 = 10578L;
+    float a5 = 37.2f;
+    double a6 = 235.8;
+    boolean a7 = true;
+    char a8 = 'С';
+    String a9 = "Hello";
+
+    public static void main(String[] args) {
+
+        System.out.println(exe1(1, 2, 8, 4));
+        System.out.println(exe2(5, 7));
+        exe3(5);
+        System.out.println(exe4(-3));
+        exe5("Ира");
+        exe6(100);
+
+    }
+
+    static int exe1(int a, int b, int c, int d) {
+
+        return a * (b + (c / d));
+    }
+
+
+    static boolean exe2(int f, int k) {
+
+
+        if (10 <= f + k && f + k <= 20) {
+
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    static void exe3(int x) {
+
+        if (x >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+
+    }
+
+    static boolean exe4(int s) {
+
+        if (s < 0) {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    static void exe5(String v) {
+
+        System.out.println("Привет, " + v);
+    }
+
+    static void exe6(int z) {
+        int l = z % 4;
+        int k = z % 100;
+        int m = z % 400;
+
+        boolean year = ((l == 0 && k != 0) || m == 0);
+        String j = year ? "Високосный" : "Не високосный";
+
+        System.out.println(j);
+
+    }
+}
+
+
+
+
+
---
+++ src/Java1HW2.java
@@ -0,0 +1,152 @@
+import java.util.Arrays;
+
+/**
+ * Java1. Homework2
+ * @authors Irina Poberey
+ * @version dated Jul 15, 2018
+ */
+public class Java1HW2 {
+
+    public static void main(String[] args) {
+
+        arr1();
+        arr2();
+        arr3();
+        arr4();
+        int[] arr = {5, 9, 1, 0, 8, 4, 7};
+        minMax(arr);
+        System.out.println(checkBalance());
+        System.out.println(Arrays.toString(shiftArray(arr, 3)));
+
+    }
+
+    static void arr1(){
+
+        int[] mas1 = {0, 1, 1, 0, 0, 1, 0};
+        for (int i = 0; i < mas1.length; i++) {
+            if (mas1[i] == 0) {
+                mas1[i] = 1;
+            } else {
+                mas1[i] = 0;
+            }
+            System.out.println(mas1[i]);
+        }
+    }
+
+    static void arr2(){
+
+        int[] mas2 = new int[8];
+
+        for (int i = 0, j = 0; i < mas2.length; i++, j = j + 3) {
+
+            mas2[i] = j;
+
+            System.out.println(mas2[i]);
+        }
+    }
+
+    static void arr3(){
+
+        int[] mas3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < mas3.length; i++) {
+            if (mas3[i] < 6) {
+                mas3[i] = mas3[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(mas3));
+
+    }
+
+    static void arr4(){
+
+        int counter = 1;
+        int[][] table = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == j || i + j == 9) {
+                    table[i][j] = 1;
+                } else {
+                    table[i][j] = counter;
+                }
+                System.out.print(table[i][j] + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
+    static void minMax(int[] arr) {
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Min = " + min + " Max = " + max);
+
+    }
+
+
+    static boolean checkBalance() {
+
+        int[] arr = {4, 2, 8, 3, 0, 2, 7, 2};
+
+        int i = 0;
+        int j = arr.length - 1;
+        int iSum = 0;
+        int jSum = 0;
+
+        while (j >= i) {
+            if (iSum <= jSum) {
+                iSum += arr[i];
+                i++;
+            } else {
+                jSum += arr[j];
+                j--;
+            }
+
+            System.out.println("iSum = " + iSum + " jSum = " + jSum);
+        }
+
+        return iSum == jSum;
+    }
+
+    static int[] shiftArray(int[] arr, int n) {
+
+        if (n > 0) {
+
+            for (int i = 0; i < n; i++) {
+
+                int temp = arr[arr.length - 1];
+
+                for (int j = arr.length - 1; j > 0; j--) {
+                    arr[j] = arr[j - 1];
+                }
+                arr[0] = temp;
+            }
+            return arr;
+        } else {
+
+            for (int i = 0; i > n; i--) {
+
+                int temp = arr[0];
+
+                for (int j = 0; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.length - 1] = temp;
+            }
+            return arr;
+        }
+    }
+}
+
+
+
